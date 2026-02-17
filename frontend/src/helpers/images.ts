1c1748a0adc06ced228e2e07b7a3ef257abc5f5c
Olivier Bossaer <olivier.bossaer@gmail.com>
2026-02-17 21:09:38 +0100

    Round optimized image

--- frontend/src/helpers/images.ts
+++ frontend/src/helpers/images.ts
@@ -1,4 +1,4 @@
-export function optimizeProfileImage(base64Input: string, maxSize = 120, quality = 0.8) {
+export function optimizeProfileImage(base64Input: string, maxSize = 120, quality = 0.8, round = true) {
   return new Promise<[string, number, number]>((resolve, reject) => {
     const img = new Image();
 
@@ -15,28 +15,34 @@ export function optimizeProfileImage(base64Input: string, maxSize = 120, quality
       }
 
       const { width, height } = img;
-
-      let newWidth = width;
-      let newHeight = height;
-
-      if (width > height) {
-        if (width > maxSize) {
-          newWidth = maxSize;
-          newHeight = (height * maxSize) / width;
-        }
-      } else {
-        if (height > maxSize) {
-          newHeight = maxSize;
-          newWidth = (width * maxSize) / height;
-        }
+      const sourceSize = Math.min(width, height);
+      const size = Math.min(sourceSize, maxSize);
+      const center = size / 2;
+      const sourceX = (width - sourceSize) / 2;
+      const sourceY = (height - sourceSize) / 2;
+      canvas.width = size;
+      canvas.height = size;
+      if (round){
+        ctx.fillStyle = "#FFFFFF";
+        ctx.fillRect(0, 0, size, size);
+        ctx.beginPath();
+        ctx.arc(center, center, center, 0, Math.PI * 2);
+        ctx.closePath();
+        ctx.clip();
       }
 
-      canvas.width = newWidth;
-      canvas.height = newHeight;
-      ctx.drawImage(img, 0, 0, newWidth, newHeight);
-      const jpegDataUri = canvas.toDataURL("image/jpeg", quality);
+      ctx.drawImage(img, sourceX, sourceY, sourceSize, sourceSize, 0, 0, size, size);
 
-      resolve([jpegDataUri, newWidth, newHeight]);
+      if (round){
+        ctx.beginPath();
+        ctx.arc(center, center, center - 1, 0, Math.PI * 2);
+        ctx.strokeStyle = "#E0E0E0";
+        ctx.lineWidth = 2 * 1.5;
+        ctx.stroke();
+      }
+
+      const jpegDataUri = canvas.toDataURL("image/jpeg", quality);
+      resolve([jpegDataUri, size, size]);
     };
 
     img.onerror = reject;
